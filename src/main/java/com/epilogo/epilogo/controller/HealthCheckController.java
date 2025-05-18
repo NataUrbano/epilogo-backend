d22037ce5b7083a3325c0361d03bf05bc31b9981
NataUrbano <u20231212696@usco.edu.co>
2025-05-18 13:36:41 -0500

    Agregado controller para despertar aplicacion en deploy

---
+++ src/main/java/com/epilogo/epilogo/controller/HealthCheckController.java
@@ -0,0 +1,23 @@
+package com.epilogo.epilogo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> healthCheck() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("timestamp", new Date());
+        return ResponseEntity.ok(response);
+    }
+}
